6dc77e9b8087a99bea39b81d6d99b9cc47352d0c
mcss55 <maqsudsafin@gmail.com>
2023-05-18 17:52:00 +0400

    ms-latest

---
+++ src/main/java/com/mcss/microserviceapp/MicroserviceAppApplication.java
@@ -0,0 +1,15 @@
+package com.mcss.microserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceAppApplication.class, args);
+    }
+
+}
